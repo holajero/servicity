8318b31ae99bbc893b9927aaf3ebdd2e177cb901
Vibesdk <vibesdk-bot@cloudflare.com>
2025-11-15 02:09:09 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,11 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add react-leaflet@^4.2.1",
+    "bun add leaflet@^1.9.4",
+    "bun add -d @types/leaflet@^1.9.12"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
